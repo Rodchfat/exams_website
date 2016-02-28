53291a99481b2c85e85b258958a8735e4761f23c
Rod c <raadce@gmail.com>
2016-02-28 02:58:37 +0000

    finishing CRUD, installing bootstrap, and adding style

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,8 @@
 class Question < ActiveRecord::Base
+    validates :description, presence: true, length: {minimum: 10, maximum: 2300}
+    validates :answer, presence: true, length: {minimum: 10, maximum: 2300}
+    validates :option1, presence: true, length: {minimum: 10, maximum: 2300}
+    validates :option2, presence: true, length: {minimum: 10, maximum: 2300}
+    validates :option3, presence: true, length: {minimum: 10, maximum: 2300}
+    validates :option4, presence: true, length: {minimum: 10, maximum: 2300}
 end
