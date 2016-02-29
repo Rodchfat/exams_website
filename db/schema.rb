a16e57a6d97cbb7fdf1dbfaec240cd1923999eca
Rod c <raadce@gmail.com>
2016-02-29 17:20:28 +0000

    this is before we do branch git

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225175349) do
+ActiveRecord::Schema.define(version: 20160229044528) do
 
   create_table "questions", force: :cascade do |t|
     t.text     "description"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20160225175349) do
     t.text     "option4"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
