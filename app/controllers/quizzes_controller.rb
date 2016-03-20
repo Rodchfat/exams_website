dc46f5923b9b30f0c8a1869deef39d331d66758d
Rod c <raadce@gmail.com>
2016-03-20 02:34:02 +0000

    this is a test commit

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,30 @@
 class QuizzesController < ApplicationController
+    def index
+    @quizzes = Quiz.all
+    end
+    
+    
+    def new 
+        @quiz = Quiz.all
+    end
+    
+    def edit
+    
+    end
+    
+    
+    def update
+    
+    end
+        
+    def create
+    @quiz = Quiz.new(quiz_params)
+    @quiz.save
+    redirect_to quiz_path(@quiz)
+    end
+    private
+    def quiz_params
+    params.require(:quiz).permit(:name, :description)
+    end
+        
 end
