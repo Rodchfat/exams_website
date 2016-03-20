db57ec95d59af32d1cbeec5568798b892a6fd09c
Rod c <raadce@gmail.com>
2016-03-20 21:12:04 +0000

    beofre brnching

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
  
-    
+ has_many :surveys   
     validates :username, presence: true,
 uniqueness: { case_sensitive: false },
 length: { minimum: 3, maximum: 25 }
